5610a410317f67c14e7bb55f6e7005d3ed741aa7
FeatherZero <wxai2324@gmail.com>
2017-03-28 10:46:13 +0800

    jssdk的dome

--- sample.php
+++ sample.php
@@ -8,9 +8,29 @@ $signPackage = $jssdk->GetSignPackage();
 <head>
   <meta charset="UTF-8">
   <title></title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+  <link href="wx_css/css/yinxiao.css" rel="stylesheet" type="text/css" />
+  <style type="text/css">
+	.logo {width:100px; height:100px; background-color:#FE8300; border-radius:50px; text-align:center; line-height:100px; color:#FFF; margin:0 auto;}
+  </style>
+  <script src="wx_css/js/jquery.min.js"></script>
 </head>
 <body>
-
+    <link href="wx_css/index/css/head.css" rel="stylesheet" type="text/css">
+    <div id="ui-header">
+      <div class="fixed top-bghead1"> <a class="ui-title" id="popmenu1">微信JSSDK接口</a> <a class="ui-btn-left_pre" href="javascript:;" onclick="history.go('-1');"></a> <a class="ui-btn-right" href="javascript:;" onclick="location.reload();"></a> </div>
+    </div>
+    <div style="height:46px;"></div>
+    <div data-role="page" >
+      <div data-role="content">
+        <div class="wtg">
+    	  <div class="logo">JSSDK</div>
+          <div class="fhtj">
+            <div class="submit"><a id='btnok' href="javascript:;">获取分享到朋友圈按钮</a></div>
+          </div>
+        </div>
+      </div>
+    </div>
 </body>
 <script src="http://res.wx.qq.com/open/js/jweixin-1.0.0.js"></script>
 <script>
